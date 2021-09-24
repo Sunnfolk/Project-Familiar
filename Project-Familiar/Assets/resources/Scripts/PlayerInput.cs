75f1e96c4e9d9203fcffd8e6bd4d6a76fdabd459
Woolrich221 <ulrikschaeffer2002@gmail.com>
2021-09-24 11:37:58 +0200

    The beggining
    Lagt til folders og input og movement script

---
+++ Project-Familiar/Assets/resources/Scripts/PlayerInput.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerInput : MonoBehaviour
+{
+   
+    [HideInInspector] public Vector2 moveVector;
+    [HideInInspector] public bool dash;
+    [HideInInspector] public bool interact;
+    void Update()
+    {
+        moveVector.x = (Keyboard.current.aKey.isPressed ? -1f : 0f) + (Keyboard.current.dKey.isPressed ? 1f : 0f);
+        moveVector.y = (Keyboard.current.aKey.isPressed ? -1f : 0f) + (Keyboard.current.wKey.isPressed ? 1f : 0f);
+
+        dash = Keyboard.current.shiftKey.wasPressedThisFrame;
+    }
+}
