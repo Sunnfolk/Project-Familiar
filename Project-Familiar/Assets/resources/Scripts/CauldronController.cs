505aa47b756f0f2c2dcb4d8c01a4733ee19fa4ec
Resonid <erlend.kaarigstad@lyse.net>
2021-09-27 17:17:53 +0200

    Basically finished cauldron and projectiles scripts

--- Project-Familiar/Assets/resources/Scripts/CauldronController.cs
+++ Project-Familiar/Assets/resources/Scripts/CauldronController.cs
@@ -3,35 +3,54 @@ using UnityEngine.InputSystem;
 
 public class CauldronController : MonoBehaviour
 {
-    [SerializeField]private float m_AngerDecreaseFq = 1;
-    [SerializeField]private float m_AngerCounter;
-    private float m_Anger;
+    private float m_AngerCounter;
     public GameObject m_Projectile;
     public Vector3 m_Position;
-    [SerializeField] private float m_TimerCounter;
-    [SerializeField] private float m_Timer =5f;
+    private float m_ShootCounter;
+    [SerializeField] private float m_ShootFq =5f;
+    [SerializeField] private float m_AngerTimer = 2f;
+    [SerializeField] private float m_Anger = 2f;
+    private bool m_Switch1;
+    private bool m_Switch2;
     void Start()
     {
-        m_TimerCounter = m_Timer;
+        m_ShootCounter = m_ShootFq;
+        m_AngerCounter = m_AngerTimer;
         m_Position = transform.position;
     }
     void Update()
     {
-        if (m_TimerCounter > 0)
+        if (m_AngerCounter > 0)
         {
-            m_TimerCounter -= Time.deltaTime;
-        }
-        if (m_TimerCounter <= 0)
-        {
-            m_TimerCounter = m_Timer;
-            m_AngerCounter--;
-            ShootProjectile();
-            print("timesup");
+            m_AngerCounter -= Time.deltaTime;
         }
 
-        if (m_AngerCounter <= m_AngerDecreaseFq)
+        if (m_AngerCounter <= 0)
         {
+            m_AngerCounter = m_AngerTimer;
             m_Anger--;
+            //print(m_Anger);
+        }
+        if (m_ShootCounter > 0)
+        {
+            m_ShootCounter -= Time.deltaTime;
+        }
+        if (m_ShootCounter <= 0)
+        {
+            m_ShootCounter = m_ShootFq;
+            ShootProjectile();
+        }
+
+        if (m_Anger <= 10 && !m_Switch1)
+        {
+            m_ShootFq -= 0.5f;
+            m_Switch1 = true;
+        }
+        
+        if (m_Anger <= 0 && !m_Switch2)
+        {
+            print("Im super angry");
+            m_Switch2 = true;
         }
         if (Keyboard.current.eKey.wasPressedThisFrame)
         {
