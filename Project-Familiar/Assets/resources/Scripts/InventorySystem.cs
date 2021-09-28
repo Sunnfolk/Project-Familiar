66bb56075aad6549915acaac19033f124105d554
Woolrich221 <ulrikschaeffer2002@gmail.com>
2021-09-28 14:34:05 +0200

    Inventory system
    Items can be picked up and dropped

---
+++ Project-Familiar/Assets/resources/Scripts/InventorySystem.cs
@@ -0,0 +1,61 @@
+using System;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class InventorySystem : MonoBehaviour
+{
+    public GameObject[] ItemPrefabs;
+    public GameObject currentItem;
+    public bool canPickup;
+    public bool canDrop;
+
+    private PlayerInput _input;
+
+   [SerializeField] private GameObject prefabObject;
+    [SerializeField] private GameObject colliderObject;
+
+    private void Start()
+    {
+        _input = GetComponent<PlayerInput>();
+    }
+
+    private void Update()
+    {
+        if (_input.interact && canPickup)
+        {
+            currentItem = prefabObject;
+            Destroy(colliderObject);
+            
+            prefabObject = null;
+            colliderObject = null;
+            canDrop = true;
+        }
+        else if (_input.interact && canDrop)
+        {
+            Instantiate(currentItem, transform.position, quaternion.identity);
+            currentItem = null;
+            canDrop = false;
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        foreach (var item in ItemPrefabs)
+        {
+            if (other.CompareTag(item.tag))
+            {
+                canPickup = true;
+                prefabObject = item;
+                colliderObject = other.gameObject;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        canPickup = false;
+        prefabObject = null;
+        colliderObject = null;
+    }
+}
