d0000b318464eab95d8c158d8fcbbad0af735f5f
Resonid <erlend.kaarigstad@lyse.net>
2021-09-28 15:10:01 +0200

    Fixed some stuff

--- Project-Familiar/Assets/resources/Scripts/PlayerController.cs
+++ Project-Familiar/Assets/resources/Scripts/PlayerController.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float m_PlayerHealth = 3;
-    public float m_PlayerScore;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
