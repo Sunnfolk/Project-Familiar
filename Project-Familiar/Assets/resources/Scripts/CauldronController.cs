58395090d4d151865a9c1d1e050b7dd6e633ba4d
Resonid <erlend.kaarigstad@lyse.net>
2021-09-28 10:08:36 +0200

    Tweaked Anger system a bit
    Its open for more tweaking

--- Project-Familiar/Assets/resources/Scripts/CauldronController.cs
+++ Project-Familiar/Assets/resources/Scripts/CauldronController.cs
@@ -3,34 +3,27 @@ using UnityEngine.InputSystem;
 
 public class CauldronController : MonoBehaviour
 {
-    private float m_AngerCounter;
     public GameObject m_Projectile;
     public Vector3 m_Position;
     private float m_ShootCounter;
     [SerializeField] private float m_ShootFq =5f;
-    [SerializeField] private float m_AngerTimer = 2f;
-    [SerializeField] private float m_Anger = 2f;
+    [SerializeField] private float m_MaxAnger = 20f;
+    private float m_Anger;
     private bool m_Switch1;
     private bool m_Switch2;
     void Start()
     {
         m_ShootCounter = m_ShootFq;
-        m_AngerCounter = m_AngerTimer;
+        m_Anger = m_MaxAnger;
         m_Position = transform.position;
     }
     void Update()
     {
-        if (m_AngerCounter > 0)
+        if (m_Anger > 0)
         {
-            m_AngerCounter -= Time.deltaTime;
+            m_Anger -= Time.deltaTime;
         }
-
-        if (m_AngerCounter <= 0)
-        {
-            m_AngerCounter = m_AngerTimer;
-            m_Anger--;
-            //print(m_Anger);
-        }
+        
         if (m_ShootCounter > 0)
         {
             m_ShootCounter -= Time.deltaTime;
@@ -38,17 +31,22 @@ public class CauldronController : MonoBehaviour
         if (m_ShootCounter <= 0)
         {
             m_ShootCounter = m_ShootFq;
-            ShootProjectile();
+            if (m_Switch1)
+            {
+                ShootProjectile();
+            }
         }
-
+        
         if (m_Anger <= 10 && !m_Switch1)
         {
             m_ShootFq -= 0.5f;
+            print("Angrier");
             m_Switch1 = true;
         }
         
         if (m_Anger <= 0 && !m_Switch2)
         {
+            m_ShootFq -= 2f;
             print("Im super angry");
             m_Switch2 = true;
         }
